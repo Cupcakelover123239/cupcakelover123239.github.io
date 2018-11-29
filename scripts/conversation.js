27fdcf5be45095e06fcca245efecedcb69532d01
Swathi Giri <swathi.giri@warriorlife.net>
2018-11-29 08:49:46 -0800

    Finished Challenge problems from Lesson #7

---
+++ scripts/conversation.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+function greeting() {
+	alert("I'm fine. Thanks for watching!");
+}
+
+function niceconvo() {
+	alert("I'm clicking on the button.");
+}
+
+function food() {
+	alert("Anything fried. HAHA!");
+}
+
+function drink() {
+	alert("Boba tea!");
+}
